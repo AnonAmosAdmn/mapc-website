411c9a92fb7ec40acb1feb5c0ba41dd2d6181d20
AnonAmosAmdn <anonamosadmn@gmail.com>
2025-07-28 14:25:50 -0500

    first commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,6 +10,7 @@ export default function Navbar() {
             <div className="text-xl font-bold text-white">MAPC</div>
             <div className="space-x-4">
                 <Link href="https://monad-ape-punks-club.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">HOME</Link>
+                <Link href="https://thirdweb.com/monad-testnet/0xA4a3B33100ddb12B4563F01152Ee55b96886ef0D" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">MINT</Link>
                 <Link href="https://mapc-staking.vercel.app" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">STAKING</Link>
                 <Link href="https://mapc-market.vercel.app" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">MARKET</Link>
             </div>
