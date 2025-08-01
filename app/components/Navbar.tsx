1f39bb43a7cecc8dea09e4308d3452c7d3c6a467
AnonAmosAmdn <anonamosadmn@gmail.com>
2025-08-01 17:35:51 -0500

    first commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     return (
         
         <nav className="w-full px-6 py-4 flex justify-between items-center border-t border-white-700 bg-black">
-            <div className="text-xl font-bold text-white">MAPC</div>
+            <div className="text-xl font-bold text-white">APE PUNKS</div>
             <div className="space-x-4">
                 <Link href="https://monad-ape-punks-club.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">HOME</Link>
                 <Link href="https://thirdweb.com/monad-testnet/0xA4a3B33100ddb12B4563F01152Ee55b96886ef0D" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">MINT</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,18 @@ export default function Home() {
     <Box position="relative" width="100%" height="100%">
       <Flex direction="column" align="center" justify="center" gap="4" py="9">
         <Heading size="9" as="h1">
-          <Text color="purple">Monad</Text> Ape Punks Club
+          <Text color="purple">Ape Punks</Text>
         </Heading>
 
         <Text size="5" color="gold">
-          MAPC brought to you by AnonAmosAdmn and MoeR87.
+          brought to you by AnonAmosAdmn and MoeR87.
         </Text>
 
         {/* Single Column Banners */}
         <Flex direction="column" gap="4" width="100%" px="4" style={{ maxWidth: '900px', minWidth: '900px' }}>
           <Box position="relative" height="350px" width="100%" mt="8" mb="8">
             <Image
-              src="/mapc-logo.png"
+              src="/ap-banner.png"
               alt="MAPC Logo"
               fill
               style={{ objectFit: "cover" }}
